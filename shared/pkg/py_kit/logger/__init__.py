aa08b3ae1f4da0010ab28db97a2889902be3348c
wexel <alex.golovin.vrn@gmail.com>
2026-04-08 23:02:33 +0300

    fix: init nop-logger in kit

--- shared/pkg/py_kit/logger/__init__.py
+++ shared/pkg/py_kit/logger/__init__.py
@@ -1,8 +1,9 @@
-from .logger import init, get_logger, with_context, debug, info, warn, error, fatal, sync
+from .logger import init, get_logger, set_nop_logger, with_context, debug, info, warn, error, fatal, sync
 
 __all__ = [
     "init",
     "get_logger",
+    "set_nop_logger",
     "with_context",
     "debug",
     "info",
