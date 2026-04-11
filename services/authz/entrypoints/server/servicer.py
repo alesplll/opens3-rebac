dab0cf122e49c64282acb7311a11b9e54860be11
wexel <alex.golovin.vrn@gmail.com>
2026-04-11 14:34:04 +0300

    feat(authz): add Action, Relation, PermServ enums to proto, drop OWNER_OF

--- services/authz/entrypoints/server/servicer.py
+++ services/authz/entrypoints/server/servicer.py
@@ -4,6 +4,32 @@ from shared.pkg.py_kit import logger
 from internal.container import Container
 from internal.types import Tuple
 
+_ACTION_TO_STR = {
+    authz_pb2.Action.ACTION_READ:   "read",
+    authz_pb2.Action.ACTION_WRITE:  "write",
+    authz_pb2.Action.ACTION_CREATE: "create",
+    authz_pb2.Action.ACTION_DELETE: "delete",
+    authz_pb2.Action.ACTION_ADMIN:  "admin",
+}
+
+_RELATION_TO_STR = {
+    authz_pb2.Relation.RELATION_MEMBER_OF:      "MEMBER_OF",
+    authz_pb2.Relation.RELATION_HAS_PERMISSION: "HAS_PERMISSION",
+    authz_pb2.Relation.RELATION_PARENT_OF:      "PARENT_OF",
+}
+
+_STR_TO_RELATION = {v: k for k, v in _RELATION_TO_STR.items()}
+
+_LEVEL_TO_STR = {
+    authz_pb2.PermissionLevel.PERMISSION_LEVEL_READ:   "read",
+    authz_pb2.PermissionLevel.PERMISSION_LEVEL_WRITE:  "write",
+    authz_pb2.PermissionLevel.PERMISSION_LEVEL_CREATE: "create",
+    authz_pb2.PermissionLevel.PERMISSION_LEVEL_DELETE: "delete",
+    authz_pb2.PermissionLevel.PERMISSION_LEVEL_ADMIN:  "admin",
+}
+
+_STR_TO_LEVEL = {v: k for k, v in _LEVEL_TO_STR.items()}
+
 
 class PermissionServiceServicer(authz_pb2_grpc.PermissionServiceServicer):
 
@@ -13,24 +39,27 @@ class PermissionServiceServicer(authz_pb2_grpc.PermissionServiceServicer):
         self._cache = container.cache
 
     def Check(self, request, context):
-        logger.debug({}, "Check RPC", subject=request.subject, action=request.action, object=request.object)
-        allowed = self._rebac.check(request.subject, request.action, request.object)
-        logger.info({}, "Check RPC done", subject=request.subject, action=request.action, object=request.object, allowed=allowed)
+        action = _ACTION_TO_STR.get(request.action, "")
+        logger.debug({}, "Check RPC", subject=request.subject, action=action, object=request.object)
+        allowed = self._rebac.check(request.subject, action, request.object)
+        logger.info({}, "Check RPC done", subject=request.subject, action=action, object=request.object, allowed=allowed)
         return authz_pb2.CheckResponse(
             allowed=allowed, reason="Neo4j ReBAC (transitive + HAS_PERMISSION)"
         )
 
     def WriteTuple(self, request, context):
-        logger.debug({}, "WriteTuple RPC", subject=request.subject, relation=request.relation, object=request.object)
-        level = request.level if request.level else None
-        tuple_ = Tuple(request.subject, request.relation, request.object, level=level)
+        relation = _RELATION_TO_STR.get(request.relation, "")
+        level = _LEVEL_TO_STR.get(request.level) if request.level else None
+        logger.debug({}, "WriteTuple RPC", subject=request.subject, relation=relation, object=request.object)
+        tuple_ = Tuple(request.subject, relation, request.object, level=level)
         success = self._rebac.write_tuple(tuple_)
         logger.info({}, "WriteTuple RPC done", success=success)
         return authz_pb2.WriteTupleResponse(success=success)
 
     def DeleteTuple(self, request, context):
-        logger.debug({}, "DeleteTuple RPC", subject=request.subject, relation=request.relation, object=request.object)
-        tuple_ = Tuple(request.subject, request.relation, request.object)
+        relation = _RELATION_TO_STR.get(request.relation, "")
+        logger.debug({}, "DeleteTuple RPC", subject=request.subject, relation=relation, object=request.object)
+        tuple_ = Tuple(request.subject, relation, request.object)
         success = self._rebac.delete_tuple(tuple_)
         logger.info({}, "DeleteTuple RPC done", success=success)
         return authz_pb2.DeleteTupleResponse(success=success)
@@ -40,9 +69,13 @@ class PermissionServiceServicer(authz_pb2_grpc.PermissionServiceServicer):
         tuples = self._rebac.read_tuples(request.subject)
         response = authz_pb2.ReadResponse()
         for t in tuples:
-            rt = response.tuples.add(subject=t.subject, relation=t.relation, object=t.object)
+            rt = response.tuples.add(
+                subject=t.subject,
+                relation=_STR_TO_RELATION.get(t.relation, authz_pb2.Relation.RELATION_UNSPECIFIED),
+                object=t.object,
+            )
             if t.level:
-                rt.level = t.level
+                rt.level = _STR_TO_LEVEL.get(t.level, authz_pb2.PermissionLevel.PERMISSION_LEVEL_UNSPECIFIED)
         logger.debug({}, "Read RPC done", count=len(tuples))
         return response
 
